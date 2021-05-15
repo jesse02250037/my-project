ce1c5c29ef14c21096caae20af3b7fa3b2d0e678
jesse02250037 <1522451270@qq.com>
2021-05-15 21:45:21 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="nav">
-
+    <router-view></router-view>
+    <tab-bar-content></tab-bar-content>
   </div>
 
 </template>
+
+
+<script>
+
+import TabBarContent from 'components/content/TabBarContent'
+
+export default {
+  name:'App',
+  components:{
+    TabBarContent
+
+  }
+}
+</script>
 
 <style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const Home=()=>import("views/home/home")
+const Category=()=>import("views/category/category")
+const Cart=()=>import("views/cart/cart")
+const Profile=()=>import("views/profile/profile")
 
 const routes = [
-
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/category',
+    component:Category
+  },
+  {
+    path:'/cart',
+    component:Cart
+  },
+  {
+    path:'/profile',
+    component:Profile
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})git push -u origin master
+})
 
 export default router
